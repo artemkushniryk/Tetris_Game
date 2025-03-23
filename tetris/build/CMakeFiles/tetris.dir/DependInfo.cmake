e9eeee68992f3158d995596048ab2a225eb404bd
artem.kushniryk <artem.kushniryk@upc.edu>
2025-03-23 19:03:20 +0100

    perf(game): add new code to improve the game

---
+++ tetris/build/CMakeFiles/tetris.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/block.cpp" "CMakeFiles/tetris.dir/src/block.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/block.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/blocks.cpp" "CMakeFiles/tetris.dir/src/blocks.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/blocks.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/colors.cpp" "CMakeFiles/tetris.dir/src/colors.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/colors.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/game.cpp" "CMakeFiles/tetris.dir/src/game.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/game.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/grid.cpp" "CMakeFiles/tetris.dir/src/grid.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/grid.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/main.cpp" "CMakeFiles/tetris.dir/src/main.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/main.cpp.o.d"
+  "/home/artem/data/PROJECTS/Tetris_Game/tetris/src/position.cpp" "CMakeFiles/tetris.dir/src/position.cpp.o" "gcc" "CMakeFiles/tetris.dir/src/position.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tetris/build/CMakeFiles/tetris.dir/src/position.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/tetris.dir/src/position.cpp.o: \
+ /home/artem/data/PROJECTS/Tetris_Game/tetris/src/position.cpp \
+ /usr/include/stdc-predef.h \
+ /home/artem/data/PROJECTS/Tetris_Game/tetris/src/../include/position.h
